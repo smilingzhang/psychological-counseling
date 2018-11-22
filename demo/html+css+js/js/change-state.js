03a9c4920d6be2b82e0483f805cc2d3713667980
mutsuo <outakashi@163.com>
2018-11-22 21:35:16 +0800

    course.html

---
+++ demo/html+css+js/js/change-state.js
@@ -0,0 +1,8 @@
+function openCommentWindow(){
+    $("#comment-input").css("display","block");
+    $("#shade").css("display","block");
+}
+function closeCommentWindow(){
+    $("#comment-input").css("display","none");
+    $("#shade").css("display","none");
+}
