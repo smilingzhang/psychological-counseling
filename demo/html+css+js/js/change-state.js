3293650ff2f4fb5486fe55089fbd524097b86f29
mutsuo <outakashi@163.com>
2018-11-22 22:40:19 +0800

    course.html 已集成video插件和评论插件

--- demo/html+css+js/js/change-state.js
+++ demo/html+css+js/js/change-state.js
@@ -6,3 +6,19 @@ function closeCommentWindow(){
     $("#comment-input").css("display","none");
     $("#shade").css("display","none");
 }
+function changeNav(obj){
+    var list = $(".dir-nav li");
+    list.each(function(index,element){
+        element.className="";
+    })
+    obj.className="active";
+    
+    if($(obj).attr("id")=="dir-nav-1"){
+        $("#directory-contain-1").css("display","display");
+        $("#directory-contain-2").css("display","none");
+    }
+    if($(obj).attr("id")=="dir-nav-2"){
+        $("#directory-contain-1").css("display","none");
+        $("#directory-contain-2").css("display","display");
+    }
+}
