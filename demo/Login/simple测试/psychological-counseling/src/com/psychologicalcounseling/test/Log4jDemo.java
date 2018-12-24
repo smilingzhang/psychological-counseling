ab3919e7077cf0d3d198596e4ea8c38885f83fd0
huihuiyouyou <912758505@qq.com>
2018-12-24 17:24:40 +0800

    feature --2018/12/24

--- demo/Login/simple测试/psychological-counseling/src/com/psychologicalcounseling/test/Log4jDemo.java
+++ demo/Login/simple测试/psychological-counseling/src/com/psychologicalcounseling/test/Log4jDemo.java
@@ -4,12 +4,10 @@ import org.apache.log4j.Logger;
 
 public class Log4jDemo {
 
-	public Log4jDemo() {
-		// TODO Auto-generated constructor stub
-	}
+	static Logger log=Logger.getLogger(Log4jDemo.class);
 	public static void main(String[] args) {
 		
-		Logger log=Logger.getLogger(Log4jDemo.class);
+		
 		for(int i=0;i<10;i++) {
 			log.info("一般信息："+i);
 			log.debug("调试信息："+i);
